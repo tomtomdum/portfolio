1ec5267b419587a475abfda1076efa89bc10f3f6
Tom <tommy.dumont.pro@gmail.com>
2024-04-06 17:16:58 -0400

    converters unit test

--- app/cryptoApp/api/page.test.tsx
+++ app/cryptoApp/api/page.test.tsx
@@ -1,8 +1,92 @@
+import { describe } from "node:test";
 import APIService from "./cryptoCoinsService";
+import { log } from "console";
 
-describe('Page', () => {
+const mockedJsonLinkEur = [
+    {
+        "price": "16.127495157934305",
+        "time": "1712428530"
+    }
+]
+
+const resMockedJsonLinkEur = [
+    {
+        "price": 16.127495157934305,
+        "time": "6/4/2024 14:35:30"
+    }
+]
+describe('test', () => {
     const api = new APIService()
-    it('renders a heading', () => {
+    it('test', () => {
         expect(api.sum(1, 1)).toEqual(2);
+    });
+});
+
+describe('unix converter', () => {
+    const api = new APIService()
+    it('converts unix to readable date', () => {
+        expect(api.formatTimestampUNIX(1712428530)).toEqual('6/4/2024 14:35:30');
+    });
+});
+
+
+describe('iso converter', () => {
+    const api = new APIService()
+    it('converts unix to readable date', () => {
+        expect(api.formatTimestampISO8601("2024-04-06T14:35:30")).toEqual("6/4/2024 14:35:30");
+    });
+});
+
+
+
+// describe('fetch crypto price history', () => {
+//     const api = new APIService()
+//     let fetchMock: jest.Mock<any, any, any>;
+//     beforeEach(() => {
+//         fetchMock = jest.fn();
+//         global.fetch = fetchMock;
+//     });
+
+//     it('returns data on successful call', async () => {
+
+
+//         fetchMock.mockResolvedValueOnce({
+//             ok: true,
+//             json: jest.fn().mockResolvedValueOnce(mockedJsonLinkEur)
+//         });
+
+//         let fnCall = await api.getCoinPriceHistory('LINK - EUR', '1');
+//         console.log("fn", fnCall);
+//         expect(fnCall).toEqual(resMockedJsonLinkEur);
+//     });
+
+// });
+
+
+describe('fetch crypto price history', () => {
+    const api = new APIService();
+    let fetchMock: jest.Mock<any, any>;
+
+    beforeEach(() => {
+        fetchMock = jest.fn();
+        global.fetch = fetchMock;
+    });
+
+    it('returns data on successful call', async () => {
+        const expectedData = [
+            {
+                price: 16.127495157934305,
+                time: '6/4/2024 14:35:30'
+            }
+        ];
+
+        fetchMock.mockResolvedValueOnce({
+            ok: true,
+            json: jest.fn().mockResolvedValueOnce(mockedJsonLinkEur)
+        });
+
+        const result = await api.getCoinPriceHistory('LINK - EUR', '1');
+
+        expect(result).toEqual(expectedData);
     });
 });
